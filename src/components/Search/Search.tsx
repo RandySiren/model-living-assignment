1c752ecc484d6824dd8cc543581010fcb505bc8c
RandySiren <sranmandeep1997@gmail.com>
2021-09-10 15:13:36 -0400

    Added search functionality

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -2,10 +2,26 @@ import './Search.scss';
 
 import { useState } from 'react';
 
-// type SearchProps = {};
+type SearchProps = {
+  setSearchResults: React.Dispatch<React.SetStateAction<string[]>>;
+};
 
-const Search = (): JSX.Element => {
+const Search = ({ setSearchResults }: SearchProps): JSX.Element => {
   const [searchQuery, setSearchQuery] = useState<string>();
+
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    // NOTE: API Keys must be stored locally, for the sake of downloading and running and simplicity we will store it directly
+    fetch(
+      `http://api.giphy.com/v1/gifs/search?api_key=jdNWJ6plFGt0nIza8kMG7znyZgVK2lEx&q=${searchQuery}&limit=20&offset=0`
+    )
+      .then((resp) => resp.json())
+      .then((resp) => {
+        const resultArray: string[] = [];
+        resp.data.map((data: any) => resultArray.push(data.embed_url));
+        setSearchResults(resultArray);
+      });
+  };
   return (
     <div className={'search'}>
       <form className={'search__form'} action='/' method='get'>
@@ -14,7 +30,11 @@ const Search = (): JSX.Element => {
           value={searchQuery}
           onChange={(e) => setSearchQuery(e.target.value)}
         />
-        <button className={'search__form__button'} type='submit'>
+        <button
+          className={'search__form__button'}
+          type='submit'
+          onClick={(e) => handleClick(e)}
+        >
           Search
         </button>
       </form>
--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -1,11 +1,19 @@
+import { useState, useEffect } from 'react';
+
 import { Header, Search, Grid } from '../components';
 import './Home.scss';
 
 const Home = (): JSX.Element => {
+  const [searchResults, setSearchResults] = useState<string[]>([]);
+
+  useEffect(() => {
+    console.log(searchResults);
+  }, [searchResults]);
+
   return (
     <div className={'home_layout'}>
       <Header text={"Manny's GIPHY GIF Searcher"} />
-      <Search />
+      <Search setSearchResults={setSearchResults} />
       <Grid />
     </div>
   );
